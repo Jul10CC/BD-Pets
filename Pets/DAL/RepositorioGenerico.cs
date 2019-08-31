819c46621173490841ea744c9c18432406c55e84
Julio Cayax <julio1cayax@outlook.es>
2019-08-30 21:37:09 -0600

    Primer Avance
    Creción de Capas, Referencias y Repositirios Genericos

---
+++ Pets/DAL/RepositorioGenerico.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Linq.Expressions;
+using System.Data.Entity.Infrastructure;
+using MODELS;
+namespace DAL
+{
+    class  RepositorioGenerico<T> : IRepositorioGenerico<T> where T : class
+    {
+        PetsJCEntities contexto;
+        public IQueryable<T> ListarTodo()
+        {
+            IQueryable<T> respuesta;
+            using (contexto = new PetsJCEntities())
+            {
+                respuesta = contexto.Set<T>().ToList().AsQueryable();
+            }//Fin del Using
+            return respuesta;
+        } //Fin de ListarTodo
+    }
+}
